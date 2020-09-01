285c30dfa837c36f847666e5592ef73a809280c5
Sidney McClean <sidney.mcclean@outlook.com>
2020-09-01 11:54:30 -0700

    set up project logo

---
+++ nba-draft-simulator/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <img className="Img" src="https://i.imgur.com/RcpxJbg.png" />
+    </div>
+  );
+}
+
+export default Header;
