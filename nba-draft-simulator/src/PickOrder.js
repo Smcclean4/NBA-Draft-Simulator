13423b4e30f48edf12b534640035f16a20c1a378
Sidney McClean <sidney.mcclean@outlook.com>
2020-09-15 15:42:13 -0700

    finished defining pick order sections

--- nba-draft-simulator/src/PickOrder.js
+++ nba-draft-simulator/src/PickOrder.js
@@ -5,26 +5,32 @@ function PickOrder() {
 
     const nbaTeam = ['Clevland', 'OKC', 'Warriors', 'Lakers']; // API teams will go here 
     // state will go here and inbetween keys
-    
+    const teamLogo = 'https://i.imgur.com/GdzXJPu.png'
     return (
-        <div className="PickOrder">
-           <div key='1' value='1'>
-
+        <div className="PickOrder flex-container">
+           <div className='teamSection' key='1' value='1'>
+                <p>1.</p>
+                <img className='teamLogo' src= {teamLogo} />
            </div>
-           <div key='2' value='2'>
-                
+           <div className='teamSection' key='2' value='2'>
+                <p>2.</p> 
+                <img className='teamLogo' src= {teamLogo} />
            </div>
-           <div key='3' value='3'>
-                
+           <div className='teamSection' key='3' value='3'>
+                <p>3.</p>  
+                <img className='teamLogo' src= {teamLogo} />
            </div>
-           <div key='4' value='4'>
-                
+           <div className='teamSection' key='4' value='4'>
+                <p>4.</p>    
+                <img className='teamLogo' src= {teamLogo} />
            </div>
-           <div key='5' value='5'>
-                
+           <div className='teamSection' key='5' value='5'>
+                <p>5.</p>  
+                <img className='teamLogo' src= {teamLogo} />
            </div>
-           <div key='6' value='6'>
-                
+           <div className='teamSection' key='6' value='6'>
+                <p>6.</p> 
+                <img className='teamLogo' src= {teamLogo} />
            </div>
         </div>
     );
