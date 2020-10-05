a4e466eae0d6a491360efd4f2c498f0d7e20b45a
Sidney McClean <sidney.mcclean@outlook.com>
2020-10-05 14:18:57 -0700

    finished setting up draftable players

--- nba-draft-simulator/src/DraftablePlayers.js
+++ nba-draft-simulator/src/DraftablePlayers.js
@@ -3,39 +3,49 @@ import './App.css';
 
 function DraftablePlayers () {
     return (
-        <div className='DraftPlayers'>
-            <ol>
-                <li class='playerName'>1.B Smith</li>
-                <li class='playerName'>2.B Smith</li>
-                <li class='playerName'>3.B Smith</li>
-                <li class='playerName'>4.B Smith</li>
-                <li class='playerName'>5.B Smith</li>
-                <li class='playerName'>6.B Smith</li>
-                <li class='playerName'>7.B Smith</li>
-                <li class='playerName'>8.B Smith</li>
-                <li class='playerName'>9.B Smith</li>
-                <li class='playerName'>10.B Smith</li>
-                <li class='playerName'>11.B Smith</li>
-                <li class='playerName'>12.B Smith</li>
-                <li class='playerName'>13.B Smith</li>
-                <li class='playerName'>14.B Smith</li>
-                <li class='playerName'>15.B Smith</li>
-                <li class='playerName'>16.B Smith</li>
-                <li class='playerName'>17.B Smith</li>
-                <li class='playerName'>18.B Smith</li>
-                <li class='playerName'>19.B Smith</li>
-                <li class='playerName'>20.B Smith</li>
-                <li class='playerName'>21.B Smith</li>
-                <li class='playerName'>22.B Smith</li>
-                <li class='playerName'>23.B Smith</li>
-                <li class='playerName'>24.B Smith</li>
-                <li class='playerName'>25.B Smith</li>
-                <li class='playerName'>26.B Smith</li>
-                <li class='playerName'>27.B Smith</li>
-                <li class='playerName'>28.B Smith</li>
-                <li class='playerName'>29.B Smith</li>
-                <li class='playerName'>30.B Smith</li>
-            </ol>
+        <div className='DraftPlayers flex-container'>
+                <div>
+                <ul>
+                <li class='playerName'>1. B Smith</li>
+                <li class='playerName'>2. B Smith</li>
+                <li class='playerName'>3. B Smith</li>
+                <li class='playerName'>4. B Smith</li>
+                <li class='playerName'>5. B Smith</li>
+                <li class='playerName'>6. B Smith</li>
+                <li class='playerName'>7. B Smith</li>
+                <li class='playerName'>8. B Smith</li>
+                <li class='playerName'>9. B Smith</li>
+                <li class='playerName'>10. B Smith</li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                <li class='playerName'>11. B Smith</li>
+                <li class='playerName'>12. B Smith</li>
+                <li class='playerName'>13. B Smith</li>
+                <li class='playerName'>14. B Smith</li>
+                <li class='playerName'>15. B Smith</li>
+                <li class='playerName'>16. B Smith</li>
+                <li class='playerName'>17. B Smith</li>
+                <li class='playerName'>18. B Smith</li>
+                <li class='playerName'>19. B Smith</li>
+                <li class='playerName'>20. B Smith</li>
+                </ul>
+                </div>
+                <div>
+                <ul>
+                <li class='playerName'>21. B Smith</li>
+                <li class='playerName'>22. B Smith</li>
+                <li class='playerName'>23. B Smith</li>
+                <li class='playerName'>24. B Smith</li>
+                <li class='playerName'>25. B Smith</li>
+                <li class='playerName'>26. B Smith</li>
+                <li class='playerName'>27. B Smith</li>
+                <li class='playerName'>28. B Smith</li>
+                <li class='playerName'>29. B Smith</li>
+                <li class='playerName'>30. B Smith</li>
+                </ul>
+                </div>
         </div>
     );
 }
