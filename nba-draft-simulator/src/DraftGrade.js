5e8a24ab59780d1d10db9917bb647046d65c7387
Sidney McClean <sidney.mcclean@outlook.com>
2020-11-04 17:45:25 -0800

    creating draft grade

--- nba-draft-simulator/src/DraftGrade.js
+++ nba-draft-simulator/src/DraftGrade.js
@@ -5,7 +5,12 @@ function DraftGrade () {
 
     return (
         <div className="two">
-
+        <div className="your-draft-grade flex-container">
+            <h2>Curent NBA Draft Grade:</h2>
+            <p className="draft-grade">B+</p>
+            <button><a href="">Reset Draft Simulator</a></button>
+            <button><a href="">Generate Current Draft Class</a></button>
+        </div>
         </div>
     );
 }
