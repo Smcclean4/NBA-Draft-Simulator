a0cb2d505b719157050f02881430e37df38423e3
Sidney McClean <sidney.mcclean@outlook.com>
2020-11-29 19:14:25 -0800

    fixing your team section

--- nba-draft-simulator/src/YourTeam_DraftGrade.js
+++ nba-draft-simulator/src/YourTeam_DraftGrade.js
@@ -14,9 +14,8 @@ function YourTeam_DraftGrade () {
                     <li className="your-pick">5. Player Name</li>
                     <li className="your-pick">6. Player Name</li>
                     <li className="your-pick">7. Player Name</li>
-                    </ul>
-                
-                
+                    </ul> 
+
                     <ul className="your-pick-ul">
                     <li className="your-pick">8. Player Name</li>
                     <li className="your-pick">9. Player Name</li>
@@ -26,10 +25,12 @@ function YourTeam_DraftGrade () {
                     <li className="your-pick">13. Player Name</li>
                     <li className="your-pick">14. Player Name</li>
                     </ul>
-                
-                <div className="your-team-name">
+
+                    <div className="your-team-name">
                         <h2>San Diego Sabers</h2>
-                </div>                                                                                            
+                    </div> 
+                
+                                                                                                          
             <div className="your-draft-grade">
                 <h2>Curent NBA Draft Grade:</h2>
                 <p className="draft-grade">B+</p>
