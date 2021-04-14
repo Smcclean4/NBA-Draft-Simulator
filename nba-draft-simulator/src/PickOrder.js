f7d5ced79a6e0c39ae21ddb0a93b3405428cb653
Sidney McClean <sidney.mcclean@outlook.com>
2021-04-14 13:47:55 -0700

    cleaning up pickorder section with names and state

--- nba-draft-simulator/src/PickOrder.js
+++ nba-draft-simulator/src/PickOrder.js
@@ -1,31 +1,52 @@
 import React from 'react';
 import './App.css';
 
-function PickOrder() {
+class PickOrder extends React.Component {
 
-    const nbaTeam = ['Clevland', 'OKC', 'Warriors', 'Lakers']; // API teams will go here 
-    // state will go here and inbetween keys
-    const teamLogo = 'https://i.imgur.com/GdzXJPu.png'
-    return (
-        <div className="PickOrder flex-container">
-           <div className='teamSection' key='1' value='1'>
-                <p>1.</p>
-                <img className='teamLogo' src= {teamLogo} />
-           </div>
-           <div className='teamSection' key='2' value='2'>
-                <p>2.</p> 
-                <img className='teamLogo' src= {teamLogo} />
-           </div>
-           <div className='teamSection' key='3' value='3'>
-                <p>3.</p>  
-                <img className='teamLogo' src= {teamLogo} />
-           </div>
-           <div className='teamSection' key='4' value='4'>
-                <p>4.</p>    
-                <img className='teamLogo' src= {teamLogo} />
-           </div>
-        </div>
-    );
+     constructor(props) {
+          super(props);
+          this.state = {
+               teamOne: {
+                    name: 'Wizards',
+                    logo: 'https://i.imgur.com/GdzXJPu.png'
+               },
+               teamTwo: {
+                    name: 'Magic',
+                    logo: 'https://i.imgur.com/GdzXJPu.png'
+               },
+               teamThree: {
+                    name: 'Rockets',
+                    logo: 'https://i.imgur.com/GdzXJPu.png'
+               },
+               teamFour: {
+                    name: 'Timberwolves',
+                    logo: 'https://i.imgur.com/GdzXJPu.png'
+               }
+          }
+     }
+
+     render() {
+          return (
+               <div className="PickOrder flex-container">
+                    <div className='teamSection' key='1' value='1'>
+                         <p>1. {this.state.teamOne.name}</p>
+                         <img className='teamLogo' src= {this.state.teamOne.logo} />
+                    </div>
+                    <div className='teamSection' key='2' value='2'>
+                         <p>2. {this.state.teamTwo.name}</p> 
+                         <img className='teamLogo' src= {this.state.teamTwo.logo} />
+                    </div>
+                    <div className='teamSection' key='3' value='3'>
+                         <p>3. {this.state.teamThree.name}</p>  
+                         <img className='teamLogo' src= {this.state.teamThree.logo} />
+                    </div>
+                    <div className='teamSection' key='4' value='4'>
+                         <p>4.{this.state.teamFour.name}</p>    
+                         <img className='teamLogo' src= {this.state.teamFour.logo} />
+                    </div>
+               </div>
+          )
+     }
 }
 
 export default PickOrder;
