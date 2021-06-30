e618cbeb0c5b4faa9b422aac0f8e3e22a203994e
Sidney McClean <sidney.mcclean@outlook.com>
2021-06-30 16:57:03 -0700

    API
    imported nba api

--- nba-draft-simulator/src/API.js
+++ nba-draft-simulator/src/API.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import axios from 'axios';
 import './App.css';
 
-async function getUser() {
-    try {
-      const response = await axios.get('https://api-nba-v1.p.rapidapi.com/seasons/')
-        console.log(response.data)
-    } catch (error) {
-        console.log(error)
-    }
+const getUser = () => {
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: "https://api-nba-v1.p.rapidapi.com/seasons/",
+  headers: {
+    'x-rapidapi-key': '8123b49e6emsh62c326ed3f752a8p1fafaejsnf08931728b5f',
+    'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
   }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+}
 
 export default getUser()
