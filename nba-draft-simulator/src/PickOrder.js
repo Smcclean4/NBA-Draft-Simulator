08745da8563d0d9777f74002d7237df2ceffcf59
Sidney McClean <sidney.mcclean@outlook.com>
2021-07-06 13:44:48 -0700

    API
    pushing data to an array and figuring out how to store it

--- nba-draft-simulator/src/PickOrder.js
+++ nba-draft-simulator/src/PickOrder.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import GetData from './API';
 
 class PickOrder extends React.Component {
 
      constructor(props) {
           super(props);
+
           this.state = {
                teamOne: {
-                    name: [],
+                    name: [<GetData />],
                     logo: []
                },
                teamTwo: {
@@ -33,15 +35,15 @@ class PickOrder extends React.Component {
                          {this.state.teamOne.logo}
                     </div>
                     <div className='teamSection' key='2' value='2'>
-                         <p>1.</p>{this.state.teamTwo.name}
+                         <p>2.</p>{this.state.teamTwo.name}
                          {this.state.teamTwo.logo}
                     </div>
                     <div className='teamSection' key='3' value='3'>
-                         <p>2.</p>{this.state.teamThree.name}
+                         <p>3.</p>{this.state.teamThree.name}
                          {this.state.teamThree.logo}
                     </div>
                     <div className='teamSection' key='4' value='4'>
-                         <p>3.</p>{this.state.teamFour.name}   
+                         <p>4.</p>{this.state.teamFour.name}   
                          {this.state.teamFour.logo}
                     </div>
                </div>
