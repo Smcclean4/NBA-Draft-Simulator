6e640f6154fe6c7146d56ea0a4a6fb0ca7f6e8aa
Sidney McClean <sidney.mcclean@outlook.com>
2021-07-25 17:29:04 -0700

    API
    created API request and post but now trying to figure out how to not repeat the team

--- nba-draft-simulator/src/PickOrder.js
+++ nba-draft-simulator/src/PickOrder.js
@@ -3,15 +3,46 @@ import './App.css';
 
 const PickOrder = () => {
      const [data, setData] = useState(null);
+     const [data2, setData2] = useState(null);
+     const [data3, setData3] = useState(null);
+     const [data4, setData4] = useState(null);
      
      useEffect( () => {
           async function fetchData(url) {
           const response = await fetch(url);
           const teams = await response.json();
-          // const rando = Math.floor(Math.random() * 30);
-          const [item] = teams.data;
-          console.log(item);
-          setData(item);
+          const rando = Math.floor(Math.random() * 29);
+          setData(teams.data[`${rando}`].city);
+          }
+          fetchData('https://www.balldontlie.io/api/v1/teams');
+     },[]);
+
+     useEffect( () => {
+          async function fetchData(url) {
+          const response = await fetch(url);
+          const teams = await response.json();
+          const rando = Math.floor(Math.random() * 29);
+          setData2(teams.data[`${rando}`].city);
+          }
+          fetchData('https://www.balldontlie.io/api/v1/teams');
+     },[]);
+
+     useEffect( () => {
+          async function fetchData(url) {
+          const response = await fetch(url);
+          const teams = await response.json();
+          const rando = Math.floor(Math.random() * 29);
+          setData3(teams.data[`${rando}`].city);
+          }
+          fetchData('https://www.balldontlie.io/api/v1/teams');
+     },[]);
+
+     useEffect( () => {
+          async function fetchData(url) {
+          const response = await fetch(url);
+          const teams = await response.json();
+          const rando = Math.floor(Math.random() * 29);
+          setData4(teams.data[`${rando}`].city);
           }
           fetchData('https://www.balldontlie.io/api/v1/teams');
      },[]);
@@ -19,92 +50,27 @@ const PickOrder = () => {
      return (
           <div className="PickOrder flex-container">
                <div className="teamSection" key="1" value="1">
-                    <p>1.</p>
-                    <div>{data}</div>
+                    <p>1.
+                    {data}
+                    </p>
                </div>
                <div className="teamSection" key="2" value="2">
-                    <p>2.</p>
-
+                    <p>2.
+                    {data2}
+                    </p>
                </div>
                <div className="teamSection" key="3" value="3">
-                    <p>3.</p>
-
+                    <p>3.
+                    {data3}
+                    </p>
                </div>
                <div className="teamSection" key="4" value="4">
-                    <p>4.</p>
-
+                    <p>4.
+                    {data4}
+                    </p>
                </div>
           </div>
    );
 };
 
 export default PickOrder 
-
-// class PickOrder extends React.Component {
-
-//      constructor(props) {
-//           super(props);
-
-//           this.state = {
-//                teamOne: {
-//                     name: "",
-//                     logo: <img alt="" src=""/>
-//                },
-//                teamTwo: {
-//                     name: "",
-//                     logo: <img alt="" src=""/>
-//                },
-//                teamThree: {
-//                     name: "",
-//                     logo: <img alt="" src=""/>
-//                },
-//                teamFour: {
-//                     name: "",
-//                     logo: <img alt="" src=""/>
-//                }
-//           }
-//      }
-
-//      render() {
-//           const axios = require('axios')
-
-//           async function setTeam(url) {
-//                try {
-//                     var response = await axios.get(url)
-//                     var randomNumber = Math.floor(Math.random() * 30)
-//                     if (randomNumber === randomNumber) {
-//                          Math.floor(Math.random() * 30)
-//                     }
-//                     var randomTeam = response.data.data[`${randomNumber}`].city
-//                     console.log(randomTeam)
-//                } catch(error) {
-//                     console.log(error)
-//                }
-//           }
-
-//           setTeam('https://www.balldontlie.io/api/v1/teams')
-
-//           return (
-//                <div className="PickOrder flex-container">
-//                     <div className='teamSection' key='1' value='1'>
-//                          <p>1.</p>{this.state.teamOne.name}
-//                          {this.state.teamOne.logo}
-//                     </div>
-//                     <div className='teamSection' key='2' value='2'>
-//                          <p>2.</p>{this.state.teamTwo.name}
-//                          {this.state.teamTwo.logo}
-//                     </div>
-//                     <div className='teamSection' key='3' value='3'>
-//                          <p>3.</p>{this.state.teamThree.name}
-//                          {this.state.teamThree.logo}
-//                     </div>
-//                     <div className='teamSection' key='4' value='4'>
-//                          <p>4.</p>{this.state.teamFour.name}   
-//                          {this.state.teamFour.logo}
-//                     </div>
-//                </div>
-//           )
-//      }
-// }
-
-// export default PickOrder;
