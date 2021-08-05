5b2764fb68d7ae00a7215fcb542919814ef1bd07
Sidney McClean <sidney.mcclean@outlook.com>
2021-08-05 13:41:46 -0700

    CheckTeam
    checking team abbreviations and adding the logos with them

---
+++ nba-draft-simulator/src/CheckTeam.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as NBAIcons from 'react-nba-logos'
+
+const CheckTeam = () => {
+    if (teams.data[`${rando}`].abbreviation === TOR) {
+         return <NBAIcons.TOR />
+    } else if (teams.data[`${rando}`].abbreviation === LAL) {
+        return <NBAIcons.LAL />
+    } else if (teams.data[`${rando}`].abbreviation === ATL) {
+        return <NBAIcons.ATL />
+    } else if (teams.data[`${rando}`].abbreviation === BOS) {
+        return <NBAIcons.BOS />
+    } else if (teams.data[`${rando}`].abbreviation === BKN) {
+        return <NBAIcons.BKN />
+    } else if (teams.data[`${rando}`].abbreviation === CHA) {
+        return <NBAIcons.CHA />
+    } else if (teams.data[`${rando}`].abbreviation === CHI) {
+        return <NBAIcons.CHI />
+    } else if (teams.data[`${rando}`].abbreviation === CLE) {
+        return <NBAIcons.CLE />
+    } else if (teams.data[`${rando}`].abbreviation === DAL) {
+        return <NBAIcons.DAL />
+    } else if (teams.data[`${rando}`].abbreviation === DEN) {
+        return <NBAIcons.DEN />
+    } else if (teams.data[`${rando}`].abbreviation === DET) {
+        return <NBAIcons.DET />
+    } else if 
+}
+
+export default CheckTeam
