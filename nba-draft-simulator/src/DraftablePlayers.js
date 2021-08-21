87823057bf7452f770609546d50525d6905f1c9f
Sidney McClean <sidney.mcclean@outlook.com>
2021-08-20 18:28:37 -0700

    ROOKIES
    Imported rookies manually

--- nba-draft-simulator/src/DraftablePlayers.js
+++ nba-draft-simulator/src/DraftablePlayers.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+// const Players = "Cade Cunningham, Jalen Green, Evan Mobley, Scottie Barnes, Jalen Suggs, Josh Giddey, Jonathan Kuminga, Franz Wagner, Davion Mitchell, Ziaire Williams, James Bouknight, Joshua Primo, Chris Duarte, Moses Moody, Corey Kispert, Alperen Sengun, Trey Murphy, Tre Mann, Kai Jones, Jalen Johnson, Keon Johnson, Isaiah Jackson, Usman Garuba, Josh Christopher, Quentin Grimes, Nah’Shon Hyland, Cameron Thomas, Jaden Springer, Day’Ron Sharpe, Santi Aldama, Isaiah Todd, Jeremiah Robinson-Earl, Jason Preston, Rokas Jokubaitis, Herbert Jones, Miles McBride, JT Thor, Ayo Dosunmu, Neemias Queta, Jared Butler, Joe Wieskamp, Isaiah Livers, Greg Brown, Kessler Edwards, Juhann Begarin, Dalano Banton, David Johnson, Sharife Cooper, Marcus Zegarowski, Filip Petrusev, BJ Boston, Luka Garza, Charles Bassey, Sandro Mamukelashvili, Aaron Wiggins, Scottie Lewis, Balsa Koprivica, Jericho Sims, RaiQuan Gray, Georgios Kalaitzakis"
+
+// const first = "Cade, Jalen, Evan, Scottie, Jalen, Josh, Jonathan, Franz, Davion, Ziaire, James, Joshua, Chris, Moses, Cory, Alperen, Trey, Tre, Kai, Jalen, Keon, Isaiah, Usman, Josh, Quentin, Nah'shon, Cameron, Jaden, Day'Ron, Santi, Isaiah, Jeremiah, Jason, Joe, Isaiah, Greg, Kessler, Juhann, Dalano, David, Sharife, Marcus, Filip, BJ, Luka, Charles, Sandro, Aaron, Scottie, Balsa, Jericho, RaiQuan, Georgios"
+
+// const last = "Cunningham, Green, Mobley, Barnes, Suggs, Giddey, Kuminga, Wagner, Mitchell, Williams, Bouknight, Primo, Duarte, Moody, Kispert, Sengun, Murphy, Mann, Jones, Johnson, Johnson, Jackson, Garuba, Christopher, Grimes, Hyland, Thomas, Springer, Sharpe, Aldama, Todd, Robinson-Earl, Preston, Jokubaitis, Jones, Mcbride, Thor, Dosunmu, Queta, Butler, Wieskamp, Livers, Brown, Edwards, Begarin, Banton, Johnson, Cooper, Zegarowski, Petrusev, Boston, Garza, Bassey, Mamukelashvili, Wiggins, Lewis, Koprivica, Sims, Gray, Kalaitzakis"
+
 class DraftablePlayers extends React.Component {
 
     constructor(props) {
@@ -18,11 +24,10 @@ class DraftablePlayers extends React.Component {
     componentDidMount() {
         async function fetchPlayerName(url) {
         const response = await fetch(url)
-        const items = await response.json()
-        console.log(items)
-        // get player data and loop throught it and place data inside of the draft board
+        const rookies = await response.json()
+        console.log(rookies)
         }
-        fetchPlayerName(`https://www.balldontlie.io/api/v1/players/next_page?=2`)
+        fetchPlayerName(`https://www.balldontlie.io/api/v1/players/?search=lamelo_ball`)
     }
 
     // create algorith for name and logo insertion 
