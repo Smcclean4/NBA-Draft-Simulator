8782156ece2d93e8b3975af54490fce4efc6607f
Sidney McClean <sidney.mcclean@outlook.com>
2021-11-29 21:40:26 -0800

    Update DraftablePlayers.js
    starting fresh and utilizing state

--- nba-draft-simulator/src/DraftablePlayers.js
+++ nba-draft-simulator/src/DraftablePlayers.js
@@ -8,42 +8,21 @@ class DraftablePlayers extends React.Component {
     // const first = ["Cade", "Jalen", "Evan", "Scottie", "Jalen", "Josh", "Jonathan", "Franz", "Davion", "Ziaire", "James", "Joshua", "Chris", "Moses", "Cory", "Alperen", "Trey", "Tre", "Kai", "Jalen", "Keon", "Isaiah", "Usman", "Josh", "Quentin", "Nah'shon", "Cameron", "Jaden", "Day'Ron", "Santi", "Isaiah", "Jeremiah", "Jason", "Joe", "Isaiah", "Greg", "Kessler", "Juhann", "Dalano", "David", "Sharife", "Marcus", "Filip", "BJ", "Luka", "Charles", "Sandro", "Aaron", "Scottie", "Balsa", "Jericho", "RaiQuan", "Georgios"]
 
     // const last = ["Cunningham", "Green", "Mobley", "Barnes", "Suggs", "Giddey", "Kuminga", "Wagner", "Mitchell", "Williams", "Bouknight", "Primo", "Duarte", "Moody", "Kispert", "Sengun", "Murphy", "Mann", "Jones", "Johnson", "Johnson", "Jackson", "Garuba", "Christopher", "Grimes", "Hyland", "Thomas", "Springer", "Sharpe", "Aldama", "Todd", "Robinson-Earl", "Preston", "Jokubaitis", "Jones", "Mcbride", "Thor", "Dosunmu", "Queta", "Butler", "Wieskamp", "Livers", "Brown", "Edwards", "Begarin", "Banton", "Johnson", "Cooper", "Zegarowski", "Petrusev", "Boston", "Garza", "Bassey", "Mamukelashvili", "Wiggins", "Lewis", "Koprivica", "Sims", "Gray", "Kalaitzakis"]
-
+ 
     constructor(props) {
         super(props);
 
         this.state = {
         count: 0,
 
-        TeamDraftee: ["Cade Cunningham", "Jalen Green", "Evan Mobley", "Scottie Barnes", "Jalen Suggs", "Josh Giddey", "Jonathan Kuminga", "Franz Wagner", "Davion Mitchell", "Ziaire Williams", "James Bouknight", "Joshua Primo", "Chris Duarte", "Moses Moody", "Corey Kispert", "Alperen Sengun", "Trey Murphy", "Tre Mann", "Kai Jones", "Jalen Johnson", "Keon Johnson", "Isaiah Jackson", "Usman Garuba", "Josh Christopher", "Quentin Grimes", "Nah’Shon Hyland", "Cameron Thomas", "Jaden Springer", "Day’Ron Sharpe", "Santi Aldama"],
+        TeamDraftee: [],
 
-        TeamDraftee2: ["Isaiah Todd", "Jeremiah Robinson-Earl", "Jason Preston", "Rokas Jokubaitis", "Herbert Jones", "Miles McBride", "JT Thor", "Ayo Dosunmu", "Neemias Queta", "Jared Butler", "Joe Wieskamp", "Isaiah Livers", "Greg Brown", "Kessler Edwards", "Juhann Begarin", "Dalano Banton", "David Johnson", "Sharife Cooper", "Marcus Zegarowski", "Filip Petrusev", "BJ Boston", "Luka Garza", "Charles Bassey", "Sandro Mamukelashvili", "Aaron Wiggins", "Scottie Lewis", "Balsa Koprivica", "Jericho Sims", "RaiQuan Gray", "Georgios Kalaitzakis"]
+        TeamDraftee2: []
         }
 
     }
-    
+
     render() {
-
-        const theTeam = this.state.TeamDraftee;
-        var thePlayersRd1 = [];
-        const interval = 2000;
-        var promise = Promise.resolve()
-
-        theTeam.forEach((theName, index) => {
-            var tl = <li className="playerName" key={index} value={index}>{`${index + 1}. ${theName}`}</li>
-            setTimeout(function() {
-                return thePlayersRd1.push(tl)
-            }, index * 2000)
-        })
-        // playerrd1 changes inside but not outside forEach?
-
-        console.log(thePlayersRd1)
-
-        const theTeam2 = this.state.TeamDraftee2;
-        const thePlayersRd2 = []
-
-        theTeam2.forEach((theName2, index) => thePlayersRd2.push(<li className="playerName" key={index} value={index}>{`${index + 1 + theTeam.length}. ${theName2}`}</li>))
-
         return (
             <div className='drafted-players'>
                 <p>Drafted Players</p>
@@ -51,13 +30,13 @@ class DraftablePlayers extends React.Component {
                     <div>
                         <ul>
                             <p className="first-round">1st Round</p>
-                            {thePlayersRd1}
+                            {}
                         </ul>
                     </div>
                     <div>
                         <ul>
                             <p className='second-round'>2nd Round</p>
-                            {thePlayersRd2}
+                            {}
                         </ul>
                     </div>
                 </div>
